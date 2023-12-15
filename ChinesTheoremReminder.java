2b8212d402a8730132197ff3e98bdb9a1683e458
djaaferyessad <76821728+djaaferyessad@users.noreply.github.com>
2023-12-15 15:12:12 +0100

    Update and rename CRT.java to ChinesTheoremReminder.java

--- ChinesTheoremReminder.java
+++ ChinesTheoremReminder.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class CRT {
+public class ChinesTheoremReminder {
     public static void main(String[] args)
     {
         Scanner myobj = new Scanner(System.in);
